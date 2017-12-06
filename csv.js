f3e60b31a517794ee5165b06c0fb39bc27b56cfa
Edin93 <il_matador@hotmail.fr>
2017-12-06 17:02:30 +0100

    readme file added, more code comments added, code reviewed

--- csv.js
+++ csv.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // declaring the rest of the variables.
 let CSV_PATH = './customer-data.csv'; // the file name.
-let newJson = '';
+let newJson;
 let allData = []; // it'll contain all the data from the csv file.
 
 // importing the data from the csv_path file.
@@ -12,15 +12,16 @@ fs.readFile(CSV_PATH,'utf-8', function (error, data){
 	if(error) console.error(err.message);
 	let newJson = data.split('\n');
 	let keys = newJson[0].split(','); // keys will contain the keys for all the objects's properties.
-	for(i=1; i<newJson.length; i++){
+	for(let i=1; i<newJson.length; i++){
 		let arr =newJson[i].split(','); // splitting all the users
 		let obj = {}; 
-		for(j=0; j<arr.length; j++){
+		for(let j=0; j<arr.length; j++){
 			obj[keys[j]] = arr[j]; // for each user's, its object's properties contain the same keys from the keys array with its approriate values.
 		}
-		allData.push(obj); // push each new object to the array containing everthing 
+		allData.push(obj); // push each new object to the array containing everthing.
 	}
-	fs.writeFile(path.join(__dirname, 'customer-data.json'), JSON.stringify(allData)) // create a json file containing all the data.
+	fs.writeFile(path.join(__dirname, 'customer-data.json'), JSON.stringify(allData)); // create a json file containing all the data.
+	console.log('All the CSV file data is added to the Customer-data json file ');
 })
 
 

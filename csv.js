be73f0137e9656e5d4e03d06e9c15709442d34ad
Edin93 <il_matador@hotmail.fr>
2017-12-06 16:47:45 +0100

    readme file added, more code comments added

--- csv.js
+++ csv.js
@@ -1,26 +1,26 @@
-const fs = require('fs')
-const path = require('path')
-let CSV_PATH = './customer-data.csv';
-let newJson = '';
-let allData = [];
+// importing node modules.
+const fs = require('fs');
+const path = require('path');
 
+// declaring the rest of the variables.
+let CSV_PATH = './customer-data.csv'; // the file name.
+let newJson = '';
+let allData = []; // it'll contain all the data from the csv file.
+
+// importing the data from the csv_path file.
 fs.readFile(CSV_PATH,'utf-8', function (error, data){
 	if(error) console.error(err.message);
 	let newJson = data.split('\n');
-	console.log(newJson);
-	console.log(newJson[0])
-	let keys = newJson[0].split(',');
-	console.log(keys);
+	let keys = newJson[0].split(','); // keys will contain the keys for all the objects's properties.
 	for(i=1; i<newJson.length; i++){
-		let arr =newJson[i].split(',');
+		let arr =newJson[i].split(','); // splitting all the users
 		let obj = {}; 
 		for(j=0; j<arr.length; j++){
-			obj[keys[j]] = arr[j];
+			obj[keys[j]] = arr[j]; // for each user's, its object's properties contain the same keys from the keys array with its approriate values.
 		}
-		allData.push(obj);
+		allData.push(obj); // push each new object to the array containing everthing 
 	}
-	console.log(allData);
-	fs.writeFile(path.join(__dirname, 'customer-data.json'), JSON.stringify(allData))
+	fs.writeFile(path.join(__dirname, 'customer-data.json'), JSON.stringify(allData)) // create a json file containing all the data.
 })
 
 
---
+++ readme.md
@@ -0,0 +1,15 @@
+# CSV TO JSON
+
+## this assignement is part of the [gomycode](https://gomycode.tn) fullstack JS cursus.
+
+## It's an intro to node assignement.
+
+1. the goal of this task is to build a node script to convert CSV files to JSON files, without using external resources.
+
+2. to better understand the program, open the csv.js file, the code is well commented.
+
+## Assignement Instructions:
+
+1. Imagine you work at a Bitcoin exchange and you have customer information coming from another source: SSN, credit card and their bitcoin numbers. However, the file format is CSV and your exchange can only support JSON.
+
+2. The 1st row of the CSV file acts as the keys/properties of the JSON object and the rest of the fields act as values. The JSON file must have an array with 1000 items. Output the JSON file in the same directory and name the file customer-data.json.
